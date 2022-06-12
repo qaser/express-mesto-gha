d3dfd09e6bfc3cb5d5c4d27a172eb7a0e67e133c
qaser <huji@mail.ru>
2022-06-12 23:44:30 +0500

    main: добавлена обработка ошибок, корректировка ошибок

--- app.js
+++ app.js
@@ -5,14 +5,6 @@ const bodyParser = require('body-parser');
 const { PORT = 3000 } = process.env; // Слушаем 3000 порт
 const app = express();
 
-class NotFoundError extends Error {
-  constructor(message) {
-    super(message);
-    this.errorMessage = message;
-    this.statusCode = 404;
-  }
-}
-
 // подключаемся к серверу mongo
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
@@ -30,8 +22,10 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
+// обработка некорректного адреса
 app.use((req, res, next) => {
-  next(new NotFoundError('Страница не найдена =('));
+  res.status(404).send({ message: 'Адрес запроса неверен' });
+  next();
 });
 
 app.listen(PORT);
