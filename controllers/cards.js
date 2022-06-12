cac8f4da4c2bae81dc81c0936c0e3fc0fb6db239
qaser <huji@mail.ru>
2022-06-12 23:25:24 +0500

    main: добавлена обработка ошибок, корректировка ошибок

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,5 +1,7 @@
 const Card = require('../models/card');
 
+const errorBadRequest = 'Передан несуществующий _id карточки';
+
 module.exports.getCards = (req, res) => {
   Card.find()
     .then((cards) => res.send(cards))
@@ -32,18 +34,18 @@ module.exports.deleteCard = (req, res) => {
     .catch((err) => res.status(400).send({ message: err.message }));
 };
 
-module.exports.likeCard = (req, res) => {
+module.exports.likeCard = (req, res, next) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $addToSet: { likes: req.user._id } },
     { new: true },
   )
     .orFail(() => {
-      res.status(404).send({ message: 'Передан несуществующий _id карточки' });
+      res.status(404).send({ message: errorBadRequest });
     })
     .then((card) => {
       if (!card) {
-        res.status(404).send({ message: 'Передан несуществующий _id карточки' });
+        res.status(404).send({ message: errorBadRequest });
       }
       res.send({ card });
     })
@@ -51,22 +53,22 @@ module.exports.likeCard = (req, res) => {
       if (err.name === 'CastError') {
         res.status(400).send({ message: 'Переданы некорректные данные для постановки/снятии лайка' });
       }
-      res.status(500).send({ message: err.message });
+      next(err);
     });
 };
 
-module.exports.dislikeCard = (req, res) => {
+module.exports.dislikeCard = (req, res, next) => {
   Card.findByIdAndUpdate(
     req.params.cardId,
     { $pull: { likes: req.user._id } }, // убрать _id из массива
     { new: true },
   )
     .orFail(() => {
-      res.status(404).send({ message: 'Передан несуществующий _id карточки' });
+      res.status(404).send({ message: errorBadRequest });
     })
     .then((card) => {
       if (!card) {
-        res.status(404).send({ message: 'Передан несуществующий _id карточки' });
+        res.status(404).send({ message: errorBadRequest });
       }
       res.status(200).send({ card });
     })
@@ -74,6 +76,6 @@ module.exports.dislikeCard = (req, res) => {
       if (err.name === 'CastError') {
         res.status(400).send({ message: 'Переданы некорректные данные для постановки/снятии лайка' });
       }
-      res.status(400).send({ message: err.message });
+      next(err);
     });
 };
