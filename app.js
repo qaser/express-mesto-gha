75149acb391de4467d7ee5c9b3f19945108fb9a0
qaser <huji@mail.ru>
2022-06-28 17:16:26 +0500

    main: добавлена аутентификация

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/cards', require('./routes/cards'));
+app.use('/users', require('./routes/users'));
 
 app.post('/signup', registerValid, createUser);
 app.post('/signin', loginValid, login);
