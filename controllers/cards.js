29a89a53867492290939b05bb185bfb992af452e
qaser <huji@mail.ru>
2022-06-30 10:43:53 +0500

    main: проверка

--- controllers/cards.js
+++ controllers/cards.js
@@ -46,8 +46,9 @@ module.exports.deleteCard = (req, res, next) => {
     .catch((err) => {
       if (err.name === 'CastError') {
         next(new BadRequestError('Введен некорректный id'));
+      } else {
+        next(err);
       }
-      next(err);
     });
 };
 
